904f48058191d2f49ee8231ffd22abf9681a9e6d
Bennett Blanco <109486714+jbblancojr@users.noreply.github.com>
2022-12-18 16:21:58 -0500

    Update bqRequests.py

--- bqRequests.py
+++ bqRequests.py
@@ -3,13 +3,13 @@ from google.cloud import bigquery
 import pandas as pd
 
 # set credentials (json key file that gives access to BigQuery)
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\jbbla\OneDrive\Documents\joinedMarathonSets\marathonweather-3d428cf9330a.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"json key file path"
 client = bigquery.Client()
 
 # query the entire table that has weather data joined to marathon data
 request = client.query(
     """
-    SELECT * FROM `marathonweather.racesWithWeather.marathons2021V2`
+    SELECT * FROM `table`
     """
 )
 
@@ -20,6 +20,6 @@ results = request.result()
 df = results.to_dataframe()
 
 # send off to folder for Excel cleaning and analysis in R
-dir = r'C:\Users\jbbla\OneDrive\Documents\joinedMarathonSets\analysis2021.csv'
+dir = r'path .csv'
 df.to_csv(dir)
 print('done')
