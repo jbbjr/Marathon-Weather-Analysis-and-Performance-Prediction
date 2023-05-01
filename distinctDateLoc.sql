48bfb7ee8f6fb492047d1cd078c5283e75ece71a
Bennett Blanco <109486714+jbblancojr@users.noreply.github.com>
2023-05-01 15:54:47 -0500

    Delete distinctDateLoc.sql

--- distinctDateLoc.sql
+++
@@ -1,7 +0,0 @@
--- Queries the necesarry data needed to gather weather data for a given year
-
-SELECT DISTINCT
-FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%B %d, %Y', Date)) as Date, 
-REPLACE(Race, " ", "_") as Race, 
-Location    
- FROM `marathonweather.allRaces.races2019Clean`
