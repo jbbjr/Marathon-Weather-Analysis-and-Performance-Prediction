8b0c43db9564d0cc3c907a7f03432292d9b0c519
Bennett Blanco <109486714+jbblancojr@users.noreply.github.com>
2023-05-01 15:54:29 -0500

    Delete weatherJoin.sql

--- weatherJoin.sql
+++
@@ -1,8 +0,0 @@
--- left joins the weather table with the existing marathon data for a given year, on key = Race 
-
-CREATE TABLE `racesWithWeather.table` AS (
-SELECT * FROM `allRaces.raceClean`
-LEFT JOIN (
-  SELECT * EXCEPT(Race, Name), Race as racename FROM `yearlyWeather.weather`
-) ON REPLACE(racename, '_', ' ') = `allRaces.racesClean`.Race
-) 
